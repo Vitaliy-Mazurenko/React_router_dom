73fd94a5d60e627143f38cb43ad8703ea45dcf2c
romannikiforov <escaro@ukr.net>
2021-10-06 21:18:24 +0300

    before homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Header from './components/Header'
+import Home from './components/Home'
+import About from './components/About'
+import Teachers from './components/Teachers'
+import Courses from './components/Courses'
+
 const App = () => (
+  <Router>
   <div className="container">
-    <h1>Hello React</h1>
+    <Header />
+    <Route exact path="/" component={Home}/>
+    
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/courses" render={() => <Courses />} />
+    <Route path="/teachers" component={Teachers} />
   </div>
+  </Router>
 );
 
 export default App;
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,21 +1,29 @@
+import {Route, NavLink} from 'react-router-dom'
+import HTML from './courses/HTML'
+import JavaScript from './courses/JavaScript'
+import CSS from './courses/CSS'
+
 const Courses = () => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2>
       <ul className="course-nav">
         <li>
-          <a href="/courses/html">HTML</a>
+          <NavLink to="/courses/html">HTML</NavLink>
         </li>
         <li>
-          <a href="/courses/css">CSS</a>
+          <NavLink to="/courses/css">CSS</NavLink>
         </li>
         <li>
-          <a href="/courses/javascript">JavaScript</a>
+          <NavLink to="/courses/javascript">JavaScript</NavLink>
         </li>
       </ul>
     </div>
 
     {/* Nested Routes */}
+    <Route path="/courses/html" component={HTML}/>
+    <Route path="/courses/css" component={CSS}/>
+    <Route path="/courses/javascript" component={JavaScript}/>
   </div>
 );
 
